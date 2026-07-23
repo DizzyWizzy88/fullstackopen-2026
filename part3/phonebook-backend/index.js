173d15e494d2c23115138d59cd0a7c4047055563
DizzyWizzy88 <dr3930397@gmail.com>
2026-07-23 13:28:09 -0500

    Include build dist and backend fixes

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const path = require('path')
 
 app.use(cors())
 
 // Ensure express.json() is called FIRST so request.body is parsed
 app.use(express.json())
 
-app.use(express.static('dist'))
+app.use(express.static(path.join(__dirname, 'dist')))
 
 //Formats JSON responses with 2-space indentation
 app.set('json spaces', 2)

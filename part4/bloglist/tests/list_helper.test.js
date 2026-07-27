84851c128aa3327a8298c2a810ef853ed9b014d2
DizzyWizzy88 <dr3930397@gmail.com>
2026-07-27 18:28:46 -0500

    4.5: favoriteBlog helper function and unit tests completed

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -63,3 +63,62 @@ describe('total likes', () => {
         assert.strictEqual(result, 24)
     })
 })
+
+describe('favorite blog', () => {
+    const listsWithOneBlog = [
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        }
+    ]
+
+    const blogs = [
+        {
+            _id: '5a422a851b54a676234d17f7',
+            title: 'React patterns',
+            author: 'Michael Chan',
+            url: 'https://reactpatterns.com/',
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Edsger W. Dijkstra',
+            url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422b3a1b54a676234d17f9',
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.cs.utexas.edu/users/UTCS/report/2011-03/dijkstra.html',
+            likes: 12,
+            __v: 0
+        }
+    ]
+
+    test('of empty list is null', () => {
+        const result = listHelper.favoriteBlog([])
+        assert.strictEqual(result, null)
+    })
+
+    test('when list has only one blog, equals that blog', () => {
+        const result = listHelper.favoriteBlog(blogs)
+        const expected = {
+            _id: '5a422b3a1b54a676234d17f9',
+            title: 'Canonical string reduction',
+            author: 'Edsger W. Dijkstra',
+            url: 'http://www.cs.utexas.edu/users/UTCS/report/2011-03/dijkstra.html',
+            likes: 12,
+            __v: 0
+        }
+
+        assert.deepStrictEqual(result, expected)
+    })
+})

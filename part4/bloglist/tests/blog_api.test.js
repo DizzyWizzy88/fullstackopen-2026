be6c7b5761d9be043294e11a2ddb3d10fe936185
DizzyWizzy88 <dr3930397@gmail.com>
2026-07-28 18:53:39 -0500

    4.8: Step 1 completed

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,50 @@
+const { test, after, before, beforeEach, describe } = require('node:test')
+const assert = require('node:assert')
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+const initialBlogs = [
+    {
+        title: 'React patterns',
+        author: 'Michael Chan',
+        url: 'https://reactpatterns.com/',
+        likes: 7
+    },
+    {
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+        likes: 5
+    }
+]
+
+// 1. Explicitly connect before tests in this file run
+before(async () => {
+    // Ensure we wait for Mongoose to connect to the TEST database
+    if (mongoose.connection.readyState === 0) {
+    await mongoose.connect(config.MONGODB_URI)
+    }
+})
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(initialBlogs)
+})
+
+describe('when there are initially some blogs saved', () => {
+    test('blogs are returned as json and correct amount is returned', async () => {
+        const response = await api
+            .get('/api/blogs')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        assert.strictEqual(response.body.length, initialBlogs.length)
+    })
+})
+
+after(async () => {
+    await mongoose.connection.close()
+})

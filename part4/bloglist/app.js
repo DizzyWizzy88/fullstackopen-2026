9bc835ff8449f01664a5c38641415ba4056b8e2d
DizzyWizzy88 <dr3930397@gmail.com>
2026-08-02 16:49:39 -0500

    4.18: Implemented token based authentication

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -7,6 +7,7 @@ const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 
 
 
@@ -25,6 +26,7 @@ mongoose.connect(config.MONGODB_URI)
     app.use(cors())
     app.use(express.json())
 
+    app.use('/api/login', loginRouter)
     app.use('/api/users', usersRouter)
     app.use('/api/blogs', blogsRouter)
 
